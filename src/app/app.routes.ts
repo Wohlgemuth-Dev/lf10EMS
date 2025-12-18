265398f325733ea8ca1a737af19dd34eecaffca1
wohlgemuth <wohlgemuth.dev@gmail.com>
2025-12-18 11:56:17 +0100

    initial and auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router';import { authGuard } from './guards/auth.guard';
+import {CallbackComponent} from "./components/callback/callback.component";
+import {EmployeeListComponent} from "./components/employee-list/employee-list.component";
+import {AppComponent} from "./app.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'callback', component: CallbackComponent },
+  { path: 'employees', component: EmployeeListComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable, of } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import {AuthService} from "../../services/auth.service";
+import {Employee} from "../../model/Employee";
+
+@Component({
+  selector: 'app-employee-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './employee-list.component.html',
+  styleUrl: './employee-list.component.css'
+})
+export class EmployeeListComponent {
+  employees$: Observable<Employee[]>;
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) {
+    this.employees$ = of([]);
+    this.fetchData();
+  }
+
+  fetchData() {
+    const token = this.authService.getAccessToken();
+    this.employees$ = this.http.get<Employee[]>('http://localhost:8089/employees', {
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+    });
+  }
+
+
+}
