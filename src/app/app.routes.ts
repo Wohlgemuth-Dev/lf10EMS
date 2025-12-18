72af6bd4e22d350d785e312e8fe65ab0d21d07ea
wohlgemuth <wohlgemuth.dev@gmail.com>
2025-12-18 12:07:18 +0100

    paar sachen vergessen, srry jungs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';import { authGuard } from './guards/aut
 import {CallbackComponent} from "./components/callback/callback.component";
 import {EmployeeListComponent} from "./components/employee-list/employee-list.component";
 import {AppComponent} from "./app.component";
+import {LoginViewComponent} from "./components/login-view/login-view.component";
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: LoginViewComponent},
+  { path: 'app', component: AppComponent },
   { path: 'callback', component: CallbackComponent },
   { path: 'employees', component: EmployeeListComponent, canActivate: [authGuard] },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: 'app' }
 ];
---
+++ src/app/components/callback/callback.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {AuthService} from "../../services/auth.service";
+
+@Component({
+  selector: 'app-callback',
+  standalone: true,
+  template: '<p>Processing login...</p>',
+})
+export class CallbackComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  async ngOnInit() {
+    const success = await this.authService.handleCallback();
+
+    if (success) {
+      this.router.navigate(['/employees']);
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
+}
